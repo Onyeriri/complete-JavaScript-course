dc23432eaae891fd4c09d0262f439a75c43b46d4
Onyeriri Victor <viktorchinonso@gmail.com>
2024-01-11 14:11:43 +0000

    complete values and variable class

---
+++ 01-fundamentals-part-1/starter/script.js
@@ -0,0 +1,15 @@
+// let greeting = "Hello World!!!";
+//
+// if(greeting === "Hello World!!!") console.log(greeting);
+//
+// // greeting = "CSS";
+//
+//
+// if(greeting === "Hello World!!!") alert(greeting);
+
+let firstName = "Chinonso";
+let middleName = "Victor";
+let lastName = "Onyeriri";
+let age = 23;
+
+alert(`My name is ${firstName} ${middleName} ${lastName}, am ${age} year's old`);
