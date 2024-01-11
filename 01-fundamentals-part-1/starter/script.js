e7d36ffde03033c2275d53d8590c9596933f5123
Onyeriri Victor <viktorchinonso@gmail.com>
2024-01-11 14:36:21 +0000

    Complete values and variables assignment

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -7,9 +7,22 @@
 //
 // if(greeting === "Hello World!!!") alert(greeting);
 
-let firstName = "Chinonso";
-let middleName = "Victor";
-let lastName = "Onyeriri";
-let age = 23;
+// let firstName = "Chinonso";
+// let middleName = "Victor";
+// let lastName = "Onyeriri";
+// let age = 23;
+//
+// alert(`My name is ${firstName} ${middleName} ${lastName}, am ${age} year's old`);
 
-alert(`My name is ${firstName} ${middleName} ${lastName}, am ${age} year's old`);
+// Values and Variables
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+//
+//     Log their values to the console.
+
+let country, continent, population;
+
+country = "Nigeria";
+continent = "Africa";
+population = 200000000
+
+alert(`The name of my Country is ${country} on ${continent} continent with population of ${population}.`);
